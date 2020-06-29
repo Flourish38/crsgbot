1bbec2fd570cb8acbc8fcf171fc74926d6f15259
Ben <brsandvik@gmail.com>
2020-06-29 13:42:57 -0700

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Test {
+    public static void main(String[] args) throws InterruptedException, LoginException {
+
+    }
+}
+
