0da51beb7a5d6f1e88d5f76ed28138706aec07e7
Ben <brsandvik@gmail.com>
2020-06-29 15:11:56 -0700

    Added PingCommand

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,12 +1,8 @@
 import listeners.CommandListener;
+import listeners.PingCommand;
 import listeners.ShutdownCommand;
-import listeners.ShutdownListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 import java.io.File;
@@ -18,27 +14,8 @@ public class Test {
         Scanner scanner = new Scanner(new File("token.txt"));
         JDA jda = JDABuilder.createDefault(scanner.nextLine()).build();
         CommandListener.setPrefix("comp ");
-        jda.addEventListener(new MyListener());
+        jda.addEventListener(new PingCommand());
         jda.addEventListener(new ShutdownCommand());
         jda.awaitReady();
-    }
-}
-
-class MyListener extends ListenerAdapter
-{
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event)
-    {
-        if (event.getAuthor().isBot()) return;
-        // We don't want to respond to other bot accounts, including ourself
-        Message message = event.getMessage();
-        String content = message.getContentRaw();
-        // getContentRaw() is an atomic getter
-        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-        if (content.equals("!ping"))
-        {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
-        }
     }
 }
---
+++ src/main/java/listeners/PingCommand.java
@@ -0,0 +1,17 @@
+package listeners;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+import java.time.Duration;
+
+public class PingCommand extends CommandListener{
+    public PingCommand(){
+        command = "ping";
+    }
+
+    @Override
+    void command(GuildMessageReceivedEvent event) {
+        long startTime = System.currentTimeMillis();
+        event.getChannel().sendMessage("Pong!").queue((a) -> a.editMessage((System.currentTimeMillis() - startTime) + "ms").queue());
+    }
+}
