d6e9656bfeaedb02dbb269fea70149dbdcb45f49
Ben <brsandvik@gmail.com>
2020-06-29 14:06:27 -0700

    Added ShutdownListener

---
+++ src/main/java/ShutdownListener.java
@@ -0,0 +1,18 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.annotation.Nonnull;
+
+public class ShutdownListener extends ListenerAdapter {
+
+    @Override
+    public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
+        if(event.getGuild().getId().equals("612357251625254953"))
+            if(event.getMessage().getMentionedUsers().size() > 0
+                    && event.getMessage().getMentionedUsers().get(0).equals(event.getJDA().getSelfUser())
+                    && event.getMessage().getContentRaw().contains("shutdown"))
+                event.getJDA().shutdown();
+    }
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,40 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) throws InterruptedException, LoginException {
-
+    public static void main(String[] args) throws InterruptedException, LoginException, FileNotFoundException {
+        Scanner scanner = new Scanner(new File("token.txt"));
+        JDA jda = JDABuilder.createDefault(scanner.nextLine()).build();
+        jda.addEventListener(new MyListener());
+        jda.addEventListener(new ShutdownListener(jda));
+        jda.awaitReady();
     }
 }
 
+class MyListener extends ListenerAdapter
+{
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.getAuthor().isBot()) return;
+        // We don't want to respond to other bot accounts, including ourself
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+        // getContentRaw() is an atomic getter
+        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
+        if (content.equals("!ping"))
+        {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
+    }
+}
